9840bf0c8d1ebd666e7be2cd70a50af2fc9ec042
Nadia <nadiawellela@gmail.com>
2023-05-17 23:04:37 -0700

    examples given

---
+++ ppid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t p_pid;
+	p_pid = getppid();
+	printf("%u\n", p_pid);
+	return (0);
+}
