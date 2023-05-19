be52cf636ccf76c2ff910fcbb27cc45d9ed5c057
Nadia <nadiawellela@gmail.com>
2023-05-19 07:48:10 -0700

    cleanup

--- shell.h
+++ shell.h
@@ -12,11 +12,5 @@ extern char **environ;
 int _putchar(char c);
 int _strcmp(char *s1, char *s2);
 void printEnv(void);
-int _fork(void);
-int _pid(void);
-int p_pid(void);
-int prompt(void);
-int _wait(void);
-int eg_exec(void);
 
 #endif
