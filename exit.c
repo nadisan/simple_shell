c799ac2c5be27962fea07980fe1f1eb6856f008b
2Kelvin <rocketmancodes02@gmail.com>
2023-05-24 09:29:53 -0700

    Add fun exitShell

--- exit.c
+++ exit.c
@@ -9,3 +9,29 @@
   */
 
 int exitShell(char *argv[], char *line)
+{
+	int exitStatus = 0;
+
+	if (argv[1] == NULL || (!_strcmp(argv[1], "0")))
+	{
+		free(line);
+		exit(0);
+	}
+
+	exitStatus = _atoi(argv[1]);
+
+	if (exitStatus != 0)
+	{
+		free(line);
+		exit(exitStatus);
+	}
+	else
+	{
+		_puts(argv[1]);
+		_puts("\n");
+		exit(2);
+	}
+
+	free(line);
+	exit(EXIT_SUCCESS);
+}
