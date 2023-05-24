82215711a2ae55e70fd023abbc8f3789318f7ca7
2Kelvin <rocketmancodes02@gmail.com>
2023-05-24 09:24:28 -0700

    Exit fun proto

---
+++ exit.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "shell.h"
+
+/**
+  * exitShell - fun exit shell with status
+  * @argv: args passed in by user
+  * @line: line
+  * Return: 0 or exit status
+  */
+
+int exitShell(char *argv[], char *line)
